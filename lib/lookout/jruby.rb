4b4b30119eea8684f065ea6355bdfa4ef1a05fe3
Ian Smith <ian.smith@mylookout.com>
2014-01-14 11:00:57 -0800

    Removed some Lookout-specific settings (re tcp4/tcp6 and SNI).

--- lib/lookout/jruby.rb
+++ lib/lookout/jruby.rb
@@ -11,12 +11,6 @@ module Lookout
 
       # JRuby YAML hack
       require 'lookout/jruby/psych'
-
-      # Oracle JRE 7 enables this which causes us all sorts of trouble with Keymaster
-      java.lang.System.set_property('jsse.enableSNIExtension', 'false')
-      # Preferring IPv4 to make sure the JVM doesn't try to open IPv6 mode TCP
-      # sockets
-      java.lang.System.set_property('java.net.preferIPv4Stack' , 'true')
     end
   end
 end
