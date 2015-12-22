844ee6c64b1377dc9ab41cabc7731a6534cc8d4c
Christian Meier <meier.kristian@gmail.com>
2015-12-22 15:47:58 +0100

    be failsafe on the Psych::Yecht::MergeKey hack

--- lib/lookout/jruby/psych.rb
+++ lib/lookout/jruby/psych.rb
@@ -18,11 +18,15 @@ if JRUBY_VERSION.start_with?("1.")
   end
 end
 
-# This hack is required because YAML is all kinds of messed up and incompatible
-# with our version of Configatron under JRuby
-module Psych
-  module Yecht
-    class MergeKey
+begin
+  # This hack is required because YAML is all kinds of messed up and incompatible
+  # with our version of Configatron under JRuby
+  module Psych
+    module Yecht
+      class MergeKey
+      end
     end
   end
+rescue Exception
+  # just be failsafe
 end
